40611132d485206cd9985daa8f833a7158e438b1
Taylor Holliday <taylor@audulus.com>
2022-08-30 14:54:51 -0700

    compute layout information in a separate pass

--- Sources/NodeEditor/PatchView.swift
+++ Sources/NodeEditor/PatchView.swift
@@ -75,13 +75,12 @@ public struct PatchView: View {
 
     func draw(_ node: Node,
               _ id: NodeID,
-              _ rect: CGRect,
               _ cx: GraphicsContext,
-              _ inputRects: inout [PortInfo: CGRect],
-              _ outputRects: inout [PortInfo: CGRect]) {
+              _ layout: Layout) {
 
         let inputs = node.inputs
         let outputs = node.outputs
+        let rect = layout.nodeRects[id]
         let pos = rect.origin
 
         let bg = Path(roundedRect: rect, cornerRadius: 5)
@@ -91,31 +90,21 @@ public struct PatchView: View {
 
         cx.draw(Text(node.name), at: pos + CGSize(width: rect.size.width/2, height: 20), anchor: .center)
 
-        var y: CGFloat = 40
         var i = 0
         for input in inputs {
-            let rect = CGRect(origin: pos + CGSize(width: portSpacing, height: y), size: portSize)
-            inputRects[.init(node: id, port: i)] = rect
+            let rect = layout.inputRects[.init(node: id, port: i)]!
             let circle = Path(ellipseIn: rect)
             cx.fill(circle, with: .color(.cyan))
-
             cx.draw(Text(input.name).font(.caption), at: rect.center + CGSize(width: (portSize.width/2 + portSpacing), height: 0), anchor: .leading)
-
-            y += portSize.height + portSpacing
             i += 1
         }
 
-        y = 40
         i = 0
         for output in outputs {
-            let rect = CGRect(origin: pos + CGSize(width: rect.size.width - portSpacing - portSize.width, height: y), size: portSize)
-            outputRects[.init(node: id, port: i)] = rect
+            let rect = layout.outputRects[.init(node: id, port: i)]!
             let circle = Path(ellipseIn: rect)
             cx.fill(circle, with: .color(.magenta))
-
             cx.draw(Text(output.name).font(.caption), at: rect.center + CGSize(width: -(portSize.width/2 + portSpacing), height: 0), anchor: .trailing)
-
-            y += portSize.height + portSpacing
             i += 1
         }
 
@@ -192,31 +181,21 @@ public struct PatchView: View {
     public var body: some View {
         Canvas { cx, size in
 
-            var nodeRects: [CGRect] = []
-            var inputRects: [PortInfo: CGRect] = [:]
-            var outputRects: [PortInfo: CGRect] = [:]
-
             cx.addFilter(.shadow(radius: 5))
 
+            let layout = self.layout
+
             var id = 0
-
-            for node in patch.nodes {
-                let shouldOffset = id == dragInfo.node || selection.contains(id)
-                let rect = rect(node: node).offset(by: (shouldOffset ? dragInfo.offset : .zero))
-                nodeRects.append(rect)
-                id += 1
-            }
-
             id = 0
             for node in patch.nodes {
-                draw(node, id, nodeRects[id], cx, &inputRects, &outputRects)
+                draw(node, id, cx, layout)
                 id += 1
             }
 
             for wire in patch.wires {
 
-                if let outputRect = outputRects[PortInfo(node: wire.from, port: wire.output)],
-                   let inputRect = inputRects[PortInfo(node: wire.to, port: wire.input)] {
+                if let outputRect = layout.outputRects[PortInfo(node: wire.from, port: wire.output)],
+                   let inputRect = layout.inputRects[PortInfo(node: wire.to, port: wire.input)] {
 
                     strokeWire(cx: cx, from: outputRect.center, to: inputRect.center)
 
