f20b7018da2898f5fb0ea4c615d699efb2307ea3
Aurelius Prochazka <aurephd@gmail.com>
2022-08-30 12:14:29 -0700

    More complex demo

--- Demo/Shared/ContentView.swift
+++ Demo/Shared/ContentView.swift
@@ -1,22 +1,50 @@
 import NodeEditor
 import SwiftUI
 
-struct ContentView: View {
+class DemoData: ObservableObject {
+    @Published var patch: Patch
 
-    @State var patch = Patch(nodes: [Node(name: "test",
-                                          position: CGPoint(x: 100, y: 100),
-                                          inputs: [Port(name: "in",
-                                                        type: .signal)],
-                                          outputs: [Port(name: "out",
-                                                         type: .signal)])], wires: [])
+    init() {
+        var nodes: [Node] = []
 
-    var body: some View {
-        PatchView(patch: $patch)
+        let generator = Node(name: "generator",
+                             position: CGPoint(x: 100, y: 100),
+                             inputs: [],
+                             outputs: [Port(name: "out",
+                                            type: .signal)])
+
+        let processor = Node(name: "processor",
+                             position: CGPoint(x: 400, y: 100),
+                             inputs: [Port(name: "in",
+                                           type: .signal)],
+                             outputs: [Port(name: "out",
+                                            type: .signal)])
+
+        let output = Node(name: "output",
+                          position: CGPoint(x: 700, y: 100),
+                          inputs: [Port(name: "in",
+                                        type: .signal)],
+                          outputs: [])
+
+        nodes.append(generator)
+        nodes.append(processor)
+        nodes.append(output)
+
+        let wire1 = Wire(from: 0, output: 0, to: 1, input: 0)
+        let wire2 = Wire(from: 1, output: 0, to: 2, input: 0)
+        let wires = [wire1, wire2]
+
+        patch = Patch(nodes: nodes, wires: wires)
+
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct ContentView: View {
+
+    @State var demoData = DemoData()
+
+    var body: some View {
+        PatchView(patch: $demoData.patch)
     }
 }
