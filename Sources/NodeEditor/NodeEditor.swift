9ad635ba8684505bb9cf02c12b023024b3672f0a
Taylor Holliday <taylor@audulus.com>
2022-08-30 11:19:30 -0700

    cleanup

--- Sources/NodeEditor/NodeEditor.swift
+++
@@ -1,6 +0,0 @@
-public struct NodeEditor {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/NodeEditorTests/NodeEditorTests.swift
+++ Tests/NodeEditorTests/NodeEditorTests.swift
@@ -6,6 +6,6 @@ final class NodeEditorTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NodeEditor().text, "Hello, World!")
+        
     }
 }
