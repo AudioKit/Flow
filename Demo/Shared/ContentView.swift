c3f0288c049a435b97205bc6d06dcd0a82fcaa51
Taylor Holliday <taylor@audulus.com>
2022-08-30 12:28:35 -0700

    Update ContentView.swift

--- Demo/Shared/ContentView.swift
+++ Demo/Shared/ContentView.swift
@@ -19,17 +19,32 @@ class DemoData: ObservableObject {
                              outputs: [Port(name: "out",
                                             type: .signal)])
 
+        let mixer = Node(name: "mixer",
+                         position: CGPoint(x: 700, y: 100),
+                         inputs: [Port(name: "in1",
+                                       type: .signal),
+                                  Port(name: "in2",
+                                       type: .signal)],
+                         outputs: [Port(name: "out",
+                                        type: .signal)])
+
         let output = Node(name: "output",
-                          position: CGPoint(x: 700, y: 100),
+                          position: CGPoint(x: 1000, y: 100),
                           inputs: [Port(name: "in",
                                         type: .signal)],
                           outputs: [])
 
-        let nodes = [generator, processor, output]
+        let nodes = [generator, processor,
+                     generator.translate(by: CGSize(width: 0, height: 100)),
+                     processor.translate(by: CGSize(width: 0, height: 100)),
+                     mixer,
+                     output]
 
-        let wire1 = Wire(from: 0, output: 0, to: 1, input: 0)
-        let wire2 = Wire(from: 1, output: 0, to: 2, input: 0)
-        let wires = [wire1, wire2]
+        let wires = [Wire(from: 0, output: 0, to: 1, input: 0),
+                     Wire(from: 1, output: 0, to: 4, input: 0),
+                     Wire(from: 2, output: 0, to: 3, input: 0),
+                     Wire(from: 3, output: 0, to: 4, input: 1),
+                     Wire(from: 4, output: 0, to: 5, input: 0)]
 
         patch = Patch(nodes: nodes, wires: wires)
 
