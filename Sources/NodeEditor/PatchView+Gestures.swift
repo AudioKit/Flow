e1b4213b761ef4d6c2964623cdb306da2ff64a05
Taylor Holliday <taylor@audulus.com>
2022-08-31 15:03:25 -0700

    re #3. Make hit testing consistent with draw order.

--- Sources/NodeEditor/PatchView+Gestures.swift
+++ Sources/NodeEditor/PatchView+Gestures.swift
@@ -29,25 +29,31 @@ extension PatchView {
         DragGesture(minimumDistance: 0)
             .updating($dragInfo) { drag, dragInfo, _ in
 
-                if let output = findOutput(point: drag.startLocation) {
-                    dragInfo = DragInfo(output: output,
-                                        origin: output.nodeIndex,
-                                        offset: drag.translation)
-                } else if let input = findInput(point: drag.startLocation) {
-                    // Is a wire attached to the input?
-                    if let attachedWire = attachedWire(inputID: input) {
-                        let offset = inputRect(node: patch.nodes[input.nodeIndex],
-                                               input: input.portIndex).center
-                        - outputRect(node: patch.nodes[attachedWire.output.nodeIndex],
-                                     output: attachedWire.output.portIndex).center
-                        + drag.translation
-                        dragInfo = DragInfo(output: attachedWire.output,
-                                            origin: attachedWire.output.nodeIndex,
-                                            offset: offset,
-                                            hideWire: attachedWire)
+                if let nodeIndex = findNode(point: drag.startLocation) {
+
+                    let node = patch.nodes[nodeIndex]
+
+                    if let output = findOutput(node: node, point: drag.startLocation) {
+                        dragInfo = DragInfo(output: OutputID(nodeIndex, output),
+                                            origin: nodeIndex,
+                                            offset: drag.translation)
+                    } else if let input = findInput(node: node, point: drag.startLocation) {
+                        // Is a wire attached to the input?
+                        if let attachedWire = attachedWire(inputID: InputID(nodeIndex, input)) {
+                            let offset = inputRect(node: node,
+                                                   input: input).center
+                            - outputRect(node: patch.nodes[attachedWire.output.nodeIndex],
+                                         output: attachedWire.output.portIndex).center
+                            + drag.translation
+                            dragInfo = DragInfo(output: attachedWire.output,
+                                                origin: attachedWire.output.nodeIndex,
+                                                offset: offset,
+                                                hideWire: attachedWire)
+                        }
+                    } else {
+                        dragInfo = DragInfo(origin: nodeIndex, offset: drag.translation)
                     }
-                } else if let nodeIndex = findNode(point: drag.startLocation) {
-                    dragInfo = DragInfo(origin: nodeIndex, offset: drag.translation)
+
                 } else {
                     dragInfo = DragInfo(selectionRect: CGRect(origin: drag.startLocation,
                                                               size: drag.translation))
@@ -55,23 +61,28 @@ extension PatchView {
             }
             .onEnded { drag in
 
-                if let output = findOutput(point: drag.startLocation) {
-                    if let input = findInput(point: drag.location) {
-                        connect(output, to: input)
-                    }
-                } else if let input = findInput(point: drag.startLocation) {
-                    // Is a wire attached to the input?
-                    if let attachedWire = attachedWire(inputID: input) {
-                        patch.wires.remove(attachedWire)
+                if let nodeIndex = findNode(point: drag.startLocation) {
+
+                    let node = patch.nodes[nodeIndex]
+
+                    if let output = findOutput(node: node, point: drag.startLocation) {
                         if let input = findInput(point: drag.location) {
-                            connect(attachedWire.output, to: input)
+                            connect(OutputID(nodeIndex, output), to: input)
                         }
-                    }
-                } else if let nodeIndex = findNode(point: drag.startLocation) {
-                    patch.nodes[nodeIndex].position += drag.translation
-                    if selection.contains(nodeIndex) {
-                        for idx in selection where idx != nodeIndex {
-                            patch.nodes[idx].position += drag.translation
+                    } else if let input = findInput(node: node, point: drag.startLocation) {
+                        // Is a wire attached to the input?
+                        if let attachedWire = attachedWire(inputID: InputID(nodeIndex, input)) {
+                            patch.wires.remove(attachedWire)
+                            if let input = findInput(point: drag.location) {
+                                connect(attachedWire.output, to: input)
+                            }
+                        }
+                    } else {
+                        patch.nodes[nodeIndex].position += drag.translation
+                        if selection.contains(nodeIndex) {
+                            for idx in selection where idx != nodeIndex {
+                                patch.nodes[idx].position += drag.translation
+                            }
                         }
                     }
                 } else {
