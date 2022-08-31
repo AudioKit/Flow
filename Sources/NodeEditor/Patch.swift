ce73a85fd3737fd6932c7b3aee61c2fd1be47a46
Taylor Holliday <taylor@audulus.com>
2022-08-31 13:01:41 -0700

    improve comment

--- Sources/NodeEditor/Patch.swift
+++ Sources/NodeEditor/Patch.swift
@@ -85,7 +85,9 @@ public struct Node: Equatable {
     }
 }
 
-/// A `Wire` is an (input, output) pair. Node graphs are often represented with the connections
+/// An (output, input) pair. Represents a connection between nodes.
+///
+/// Node graphs are often represented with the connections
 /// on the inputs instead of a separate set, which doesn't allow multiple inputs connected to
 /// a single node. Our data model allows arbitrary connections, though we don't yet support
 /// editing of arbitrary connection graphs (an input can only have one wire).
