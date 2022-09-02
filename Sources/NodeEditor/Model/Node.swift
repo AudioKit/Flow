173e43110964871c95967f32647a94094da18212
Aurelius Prochazka <aurephd@gmail.com>
2022-09-02 15:27:33 -0700

    Made funcs accessible

--- Sources/NodeEditor/Model/Node.swift
+++ Sources/NodeEditor/Model/Node.swift
@@ -37,7 +37,7 @@ public struct Node: Equatable {
     }
 
     /// Calculates the boudning rectangle for a node.
-    func rect(layout: LayoutConstants) -> CGRect {
+    public func rect(layout: LayoutConstants) -> CGRect {
         let maxio = CGFloat(max(inputs.count, outputs.count))
         let size = CGSize(width: layout.nodeWidth, height: CGFloat(maxio * 30 + layout.nodeTitleHeight))
 
@@ -45,14 +45,14 @@ public struct Node: Equatable {
     }
 
     /// Calculates the bounding rectangle for an input port (not including the name).
-    func inputRect(input: PortIndex, layout: LayoutConstants) -> CGRect {
+    public func inputRect(input: PortIndex, layout: LayoutConstants) -> CGRect {
         let y = layout.nodeTitleHeight + CGFloat(input) * (layout.portSize.height + layout.portSpacing)
         return CGRect(origin: position + CGSize(width: layout.portSpacing, height: y),
                       size: layout.portSize)
     }
 
     /// Calculates the bounding rectangle for an output port (not including the name).
-    func outputRect(output: PortIndex, layout: LayoutConstants) -> CGRect {
+    public func outputRect(output: PortIndex, layout: LayoutConstants) -> CGRect {
         let y = layout.nodeTitleHeight + CGFloat(output) * (layout.portSize.height + layout.portSpacing)
         return CGRect(origin: position + CGSize(width: layout.nodeWidth - layout.portSpacing - layout.portSize.width, height: y),
                       size: layout.portSize)
