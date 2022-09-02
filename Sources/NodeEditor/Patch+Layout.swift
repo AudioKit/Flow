1fd85dd32a66c9d4121f79e0e241aa15ae13a4ea
Taylor Holliday <taylor@audulus.com>
2022-09-02 13:16:40 -0700

    add functions

--- Sources/NodeEditor/Patch+Layout.swift
+++ Sources/NodeEditor/Patch+Layout.swift
@@ -12,10 +12,25 @@ public struct LayoutConstants {
 
 public extension Node {
 
+    /// Calculates the boudning rectangle for a node.
     func rect(layout: LayoutConstants) -> CGRect {
         let maxio = max(inputs.count, outputs.count)
         let size = CGSize(width: layout.nodeWidth, height: CGFloat(maxio * 30 + 40))
 
         return CGRect(origin: position, size: size)
+    }
+
+    /// Calculates the bounding rectangle for an input port (not including the name).
+    func inputRect(input: PortIndex, layout: LayoutConstants) -> CGRect {
+        let y = layout.nodeTitleHeight + CGFloat(input) * (layout.portSize.height + layout.portSpacing)
+        return CGRect(origin: position + CGSize(width: layout.portSpacing, height: y),
+                      size: layout.portSize)
+    }
+
+    /// Calculates the bounding rectangle for an output port (not including the name).
+    func outputRect(output: PortIndex, layout: LayoutConstants) -> CGRect {
+        let y = layout.nodeTitleHeight + CGFloat(output) * (layout.portSize.height + layout.portSpacing)
+        return CGRect(origin: position + CGSize(width: layout.nodeWidth - layout.portSpacing - layout.portSize.width, height: y),
+                      size: layout.portSize)
     }
 }
