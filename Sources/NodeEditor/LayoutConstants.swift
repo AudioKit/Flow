ddb7b73c3779d92820082ba35f15e7a83034777f
Taylor Holliday <taylor@audulus.com>
2022-09-02 14:13:39 -0700

    move code

---
+++ Sources/NodeEditor/LayoutConstants.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+public struct LayoutConstants {
+    let portSize = CGSize(width: 20, height: 20)
+    let portSpacing: CGFloat = 10
+    let nodeWidth: CGFloat = 200
+    let nodeTitleHeight: CGFloat = 40
+
+    public init() {}
+}
--- Sources/NodeEditor/Patch.swift
+++ Sources/NodeEditor/Patch.swift
@@ -14,4 +14,35 @@ public struct Patch: Equatable {
         self.nodes = nodes
         self.wires = wires
     }
+
+    enum HitTestResult {
+        case node(NodeIndex)
+        case input(NodeIndex, PortIndex)
+        case output(NodeIndex, PortIndex)
+        case background
+    }
+
+    /// Hit test a point against the whole patch.
+    func hitTest(point: CGPoint, layout: LayoutConstants) -> HitTestResult {
+
+        for (nodeIndex, node) in nodes.enumerated().reversed() {
+            for (inputIndex, _) in node.inputs.enumerated() {
+                if node.inputRect(input: inputIndex, layout: layout).contains(point) {
+                    return .input(nodeIndex, inputIndex)
+                }
+            }
+            for (outputIndex, _) in node.outputs.enumerated() {
+                if node.outputRect(output: outputIndex, layout: layout).contains(point) {
+                    return .output(nodeIndex, outputIndex)
+                }
+            }
+
+            if node.rect(layout: layout).contains(point) {
+                return .node(nodeIndex)
+            }
+        }
+
+        return .background
+    }
 }
+
