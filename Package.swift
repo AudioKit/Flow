dfcde433702ae35f2cfbe121ec5fd2e772649bac
Aurelius Prochazka <aurephd@gmail.com>
2022-09-02 16:19:23 -0700

    Tightened up Package.swift

--- Package.swift
+++ Package.swift
@@ -1,32 +1,14 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "NodeEditor",
     platforms: [.macOS(.v12), .iOS(.v15)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "NodeEditor",
-            targets: ["NodeEditor"]
-        ),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/audulus/vger", branch: "main"),
-    ],
+    products: [.library(name: "NodeEditor", targets: ["NodeEditor"])],
+    dependencies: [.package(url: "https://github.com/audulus/vger", branch: "main")],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "NodeEditor",
-            dependencies: ["vger"]
-        ),
-        .testTarget(
-            name: "NodeEditorTests",
-            dependencies: ["NodeEditor"]
-        ),
+        .target(name: "NodeEditor", dependencies: ["vger"]),
+        .testTarget(name: "NodeEditorTests", dependencies: ["NodeEditor"]),
     ]
 )
