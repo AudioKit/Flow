7b99d105861554af289983374a60d23d1f7cf8fc
Taylor Holliday <taylor@audulus.com>
2022-09-02 14:12:08 -0700

    move code

--- Sources/NodeEditor/Patch.swift
+++ Sources/NodeEditor/Patch.swift
@@ -15,19 +15,3 @@ public struct Patch: Equatable {
         self.wires = wires
     }
 }
-
-/// An (output, input) pair. Represents a connection between nodes.
-///
-/// Node graphs are often represented with the connections
-/// on the inputs instead of a separate set, which doesn't allow multiple inputs connected to
-/// a single node. Our data model allows arbitrary connections, though we don't yet support
-/// editing of arbitrary connection graphs (an input can only have one wire).
-public struct Wire: Equatable, Hashable {
-    var output: OutputID
-    var input: InputID
-
-    public init(from: OutputID, to: InputID) {
-        output = from
-        input = to
-    }
-}
