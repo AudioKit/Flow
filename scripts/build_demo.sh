c51af08346f462b9f80da63262c719efdb112235
Brian Drelling <brian@drelling.com>
2022-09-03 13:07:30 -0500

    Removed feature_request.yml

--- scripts/build_demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Our first argument is the scheme.
-scheme=$1
-
-xcodebuild clean build -project Demo/FlowDemo.xcodeproj -scheme 'FlowDemo (iOS)' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
-
-# -sdk iphonesimulator -scheme FlowDemo -arch x86_64 ONLY_ACTIVE_ARCH=YES CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" clean build | xcpretty -c
