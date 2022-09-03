71354d60880d7a3927814114dafe3cfee222359f
Steffan Andrews <stef@orchetect.com>
2022-09-03 11:27:48 -0700

    Added compiler disambiguation for `Node` inits

---
+++ Tests/FlowTests/NodeTests.swift
@@ -0,0 +1,16 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/Flow/
+
+@testable import Flow
+import XCTest
+
+final class NodeTests: XCTestCase {
+    
+    /// This test ensures disambiguation for identical `Node` init signature overloads
+    /// where inputs and outputs are empty.
+    /// It will throw a compiler error if it cannot determine which to use.
+    /// No logic testing is necessary here.
+    func testNodeInitDisambiguation() throws {
+        _ = Node(name: "Name")
+        _ = Node(name: "Name", position: .zero, inputs: [], outputs: [])
+    }
+}
