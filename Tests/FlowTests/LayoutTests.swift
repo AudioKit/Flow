8283f1c89c4ba537e98074f972ee6738a539d9c5
Aurelius Prochazka <aurephd@gmail.com>
2022-09-12 22:28:04 -0700

    cleanup

--- Tests/FlowTests/LayoutTests.swift
+++ Tests/FlowTests/LayoutTests.swift
@@ -7,10 +7,8 @@ final class LayoutTests: XCTestCase {
     func testNodeRects() throws {
         let processor = Node(name: "processor",
                              position: CGPoint(x: 400, y: 100),
-                             inputs: [Port(name: "in",
-                                           type: .signal)],
-                             outputs: [Port(name: "out",
-                                            type: .signal)])
+                             inputs: [Port(name: "in", type: .signal)],
+                             outputs: [Port(name: "out", type: .signal)])
 
         XCTAssertEqual(processor.rect(layout: LayoutConstants()),
                        CGRect(origin: processor.position, size: CGSize(width: 200, height: 70)))
