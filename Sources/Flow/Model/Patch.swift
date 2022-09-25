63c119a5af471d057dd26b3d5a554b6b338b4ccc
Taylor Holliday <taylor@audulus.com>
2022-09-25 14:15:55 -0700

    update comment

--- Sources/Flow/Model/Patch.swift
+++ Sources/Flow/Model/Patch.swift
@@ -7,7 +7,7 @@ import Foundation
 ///
 /// Write a function to generate a `Patch` from your own data model
 /// as well as a function to update your data model when the `Patch` changes.
-/// Use SwiftUI's `onChange(of:)` to monitor changes.
+/// Use SwiftUI's `onChange(of:)` to monitor changes, or use `NodeEditor.onNodeAdded`, etc.
 public struct Patch: Equatable {
     public var nodes: [Node]
     public var wires: Set<Wire>
