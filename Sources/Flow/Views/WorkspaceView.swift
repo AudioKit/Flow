559f5dc4beb6225be15e801398102f8b9da03bc7
Taylor Holliday <taylor@audulus.com>
2022-09-26 17:16:19 -0700

    Use PanView instead of a CoordinatorObject

--- Sources/Flow/Views/WorkspaceView.swift
+++ Sources/Flow/Views/WorkspaceView.swift
@@ -106,86 +106,38 @@ class PanView: NSView {
             print("two fingers on trackapd")
         }
     }
+
+    @objc func panGesture(sender: NSPanGestureRecognizer) {
+        print("pan at location: \(sender.location(in: self))")
+        let t = sender.translation(in: self)
+        pan.width += t.x / zoom
+        pan.height -= t.y / zoom
+
+        // Reset translation.
+        sender.setTranslation(CGPoint.zero, in: nil)
+    }
+
+    @objc func zoomGesture(sender: NSMagnificationGestureRecognizer) {
+        print("pinch at location: \(sender.location(in: self)), scale: \(sender.magnification)")
+
+        let p = sender.location(in: self).size
+
+        let newZoom = sender.magnification * zoom
+
+        let pLocal = p * (1.0 / zoom) - pan
+        let newPan = p * (1.0 / newZoom) - pLocal
+
+        pan = newPan
+        zoom = newZoom
+
+        // Reset scale.
+        sender.magnification = 1.0
+    }
+
+    weak var optionPanRecognizer: NSGestureRecognizer?
 }
 
-struct WorkspaceView: NSViewRepresentable {
-    @Binding var pan: CGSize
-    @Binding var zoom: Double
-
-    class Coordinator: NSObject {
-        @Binding var pan: CGSize
-        @Binding var zoom: Double
-
-        init(pan: Binding<CGSize>, zoom: Binding<Double>) {
-            _pan = pan
-            _zoom = zoom
-        }
-
-        @objc func panGesture(sender: NSPanGestureRecognizer) {
-            print("pan at location: \(sender.location(in: nil))")
-            let t = sender.translation(in: nil)
-            pan.width += t.x / zoom
-            pan.height -= t.y / zoom
-
-            // Reset translation.
-            sender.setTranslation(CGPoint.zero, in: nil)
-        }
-
-        @objc func zoomGesture(sender: NSMagnificationGestureRecognizer) {
-            print("pinch at location: \(sender.location(in: nil)), scale: \(sender.magnification)")
-
-            let p = sender.location(in: nil).size
-
-            let newZoom = sender.magnification * zoom
-
-            let pLocal = p * (1.0 / zoom) - pan
-            let newPan = p * (1.0 / newZoom) - pLocal
-
-            pan = newPan
-            zoom = newZoom
-
-            // Reset scale.
-            sender.magnification = 1.0
-        }
-
-        weak var optionPanRecognizer: NSGestureRecognizer?
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(pan: $pan, zoom: $zoom)
-    }
-
-    func makeNSView(context: Context) -> NSView {
-        let view = PanView(pan: $pan, zoom: $zoom)
-
-        let coordinator = context.coordinator
-
-        let panRecognizer = NSPanGestureRecognizer(target: coordinator,
-                                                   action: #selector(Coordinator.panGesture(sender:)))
-        view.addGestureRecognizer(panRecognizer)
-        panRecognizer.buttonMask = 2
-        panRecognizer.delegate = coordinator
-
-        let optionPanRecognizer = NSPanGestureRecognizer(target: coordinator,
-                                                   action: #selector(Coordinator.panGesture(sender:)))
-        view.addGestureRecognizer(optionPanRecognizer)
-        optionPanRecognizer.delegate = coordinator
-        coordinator.optionPanRecognizer = optionPanRecognizer
-
-        let zoomRecognizer = NSMagnificationGestureRecognizer(target: coordinator,
-                                                              action: #selector(Coordinator.zoomGesture(sender:)))
-        view.addGestureRecognizer(zoomRecognizer)
-        zoomRecognizer.delegate = coordinator
-
-        return view
-    }
-
-    func updateNSView(_: NSView, context _: Context) {
-        // Do nothing.
-    }
-}
-
-extension WorkspaceView.Coordinator: NSGestureRecognizerDelegate {
+extension PanView: NSGestureRecognizerDelegate {
 
     func gestureRecognizerShouldBegin(_ gestureRecognizer: NSGestureRecognizer) -> Bool {
         if gestureRecognizer == optionPanRecognizer {
@@ -198,6 +150,38 @@ extension WorkspaceView.Coordinator: NSGestureRecognizerDelegate {
                            shouldRecognizeSimultaneouslyWith _: NSGestureRecognizer) -> Bool
     {
         return true
+    }
+}
+
+struct WorkspaceView: NSViewRepresentable {
+    @Binding var pan: CGSize
+    @Binding var zoom: Double
+
+    func makeNSView(context: Context) -> NSView {
+        let view = PanView(pan: $pan, zoom: $zoom)
+
+        let panRecognizer = NSPanGestureRecognizer(target: view,
+                                                   action: #selector(PanView.panGesture(sender:)))
+        view.addGestureRecognizer(panRecognizer)
+        panRecognizer.buttonMask = 2
+        panRecognizer.delegate = view
+
+        let optionPanRecognizer = NSPanGestureRecognizer(target: view,
+                                                   action: #selector(PanView.panGesture(sender:)))
+        view.addGestureRecognizer(optionPanRecognizer)
+        optionPanRecognizer.delegate = view
+        view.optionPanRecognizer = optionPanRecognizer
+
+        let zoomRecognizer = NSMagnificationGestureRecognizer(target: view,
+                                                              action: #selector(PanView.zoomGesture(sender:)))
+        view.addGestureRecognizer(zoomRecognizer)
+        zoomRecognizer.delegate = view
+
+        return view
+    }
+
+    func updateNSView(_: NSView, context _: Context) {
+        // Do nothing.
     }
 }
 
